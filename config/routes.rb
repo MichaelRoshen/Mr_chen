2a7fb8980215d7d621cf8d603d57f49690ce3606
MichaelRoshen <michael_roshen@163.com>
2013-01-07 06:32:14 -0800

    modify the home page and commit the databases

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MrChen::Application.routes.draw do
+  get "home/index"
+
   resources :cost_bills
 
 
@@ -60,7 +62,7 @@ MrChen::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
