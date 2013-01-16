a78dc09758068df18f78660558cf1ff82fad0cd1
MichaelRoshen <michael_roshen@163.com>
2013-01-16 06:39:48 -0800

    complete create come_in_bill js

--- app/assets/javascripts/my_effect.js
+++ app/assets/javascripts/my_effect.js
@@ -6,22 +6,27 @@ $(document).ready(function(){
     $("#bt_create_cib").click(function(){
         alert("hello");
         var v_money = come_in_bill_money.val();
-        var v_come_in_type = come_in_type.val();  
+        var v_come_in_type_id = come_in_type.val();
+        var v_come_in_type = $("#come_in_type_id").find("option:selected").text();  
         var v_date= billdate.val();  
         var v_remarks = remarks.val();    
         $.post("create_come_in_bill", {  
            money: v_money, 
-           type: v_come_in_type, 
+           type: v_come_in_type_id, 
            date: v_date,
            remarks: v_remarks  
        },  
        function(data) {  
         var come_in_bill = "";
-        come_in_bill += "<tr><td>" + "" +"</td>"
+        come_in_bill += "<tr><td>" + v_come_in_type +"</td>"
         come_in_bill += "<td>" + data["money"] +"</td>"
-        come_in_bill += "<td>" + data["billdate"] +"</td></tr>"
+        come_in_bill += "<td>" + data["billdate"] +"</td>"
+        come_in_bill += "<td><a class='btn btn-mini' href='/come_in_bills/"+ data["id"]+"/edit'><span class='translation_missing' title='translation missing: en.helpers.links.edit'>Edit</span></a>"
+        come_in_bill += " <a class='btn btn-mini btn-danger' href='/come_in_bills/" +data["id"]+ "' data-confirm='Are you sure?' data-method='delete' rel='nofollow'><span class='translation_missing' title='translation missing: en.helpers.links.destroy'>Destroy</span></a></td></tr>"
+        
         $("#table_cil").append(come_in_bill);
     },  
     "json")  
     });
 });
+
